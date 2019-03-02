5e5da8e4a52dd0cce74b89d49e49c45407704f11
XSA_DEV <XSA_DEV@unknown>
2019-03-02 00:04:28 +0000

    new customer ui5 app + backend update

---
+++ customer/resources/webapp/model/models.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function(JSONModel, Device) {
+	"use strict";
+
+	var baseURl = "https://hxehost:51030";
+
+	return {
+
+		createDeviceModel: function() {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+
+	/*	createRestaurantsModel: function() {
+			return new ODataModel(baseURl + "/xsodata/Restaurants.xsodata", {
+				defaultBindingMode: sap.ui.model.BindingMode.TwoWay,
+				defaultUpdateMethod: sap.ui.model.odata.UpdateMethod.Put
+			});
+		},
+*/
+	};
+});
